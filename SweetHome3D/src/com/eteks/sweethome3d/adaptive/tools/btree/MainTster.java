7a126c71a461697f609bfa0447700e0c9fb945b0
edoardo90 <edoardo.pasi@mail.polimi.it>
2015-03-20 15:58:28 +0000

    better btree implementation

---
+++ SweetHome3D/src/com/eteks/sweethome3d/adaptive/tools/btree/MainTster.java
@@ -0,0 +1,89 @@
+package com.eteks.sweethome3d.adaptive.tools.btree;
+
+public class MainTster {
+
+  public static void main(String [] args)
+  {
+    MainTster mt = new MainTster();
+    mt.x();
+  }
+  
+  public void x()
+  {
+    
+    BTree<Dog> dogs = new BTree<Dog>();
+    
+    dogs.insert(new Dog("Rex"));
+    dogs.insert(new Dog("Fido"));
+    dogs.insert(new Dog("Bob"));
+    dogs.insert(new Dog("T-Rex"));
+    
+    dogs.toString();
+    
+    Dog rex = dogs.getNode(new Dog("REX"));
+    System.out.println(rex);
+    
+    
+  }
+  
+  public class Dog implements Comparable<Dog>
+  {
+    private String name;
+    
+    public Dog(String name)
+    {
+      this.name = name;
+    }
+    
+    @Override
+    public String toString()
+    {
+      return this.name + ", the dog!";
+    }
+    
+    public int compareTo(Dog o) {
+     
+      return this.name.toUpperCase().compareTo(o.name.toUpperCase());
+    }
+
+    @Override
+    public int hashCode() {
+      final int prime = 31;
+      int result = 1;
+      result = prime * result + getOuterType().hashCode();
+      result = prime * result + ((this.name == null)
+          ? 0
+          : this.name.hashCode());
+      return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (this == obj)
+        return true;
+      if (obj == null)
+        return false;
+      if (getClass() != obj.getClass())
+        return false;
+      Dog other = (Dog)obj;
+      if (!getOuterType().equals(other.getOuterType()))
+        return false;
+      if (this.name == null) {
+        if (other.name != null)
+          return false;
+      } else if (!this.name.toUpperCase().equals(other.name.toUpperCase()))
+        return false;
+      return true;
+    }
+
+    private MainTster getOuterType() {
+      return MainTster.this;
+    }
+    
+    
+    
+    
+  }
+  
+
+}
